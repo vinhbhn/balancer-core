1d0cd57f1005912a790fe68384de58b984e1a5fe
vinhbhn <bvinhhn@gmail.com>
2021-05-15 20:49:20 +0700

    deploy balancer token on mumbai

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -1,10 +1,12 @@
 const TMath = artifacts.require('TMath');
-const BToken = artifacts.require('BToken');
+const BPool = artifacts.require('BPool');
 const BFactory = artifacts.require('BFactory');
 
 module.exports = async function (deployer, network, accounts) {
     if (network === 'development' || network === 'coverage') {
         deployer.deploy(TMath);
     }
+    deployer.deploy(BToken);
+    deployer.deploy(BPool);
     deployer.deploy(BFactory);
 };
